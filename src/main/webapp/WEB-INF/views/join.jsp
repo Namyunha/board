50acf5e390a9f277c65eb9e7b0c70157909dc2dd
Namyunha <ajs998@naver.com>
2023-05-03 05:33:44 +0900

    update duChek

--- src/main/webapp/WEB-INF/views/join.jsp
+++ src/main/webapp/WEB-INF/views/join.jsp
@@ -29,28 +29,7 @@
     </ul>
 </header>
 <script>
-    const join = () => {
-        main_form.submit();
-    }
-    const checkId = () => {
-        const checkId = document.querySelector("#memberId").value;
-        const checkIdResult = document.querySelector("#checkIdResult");
-        $.ajax({
-            type: "post",
-            url: "/search",
-            data: {
-                "checkId": checkId
-            },
-            success: function(){
-                checkIdResult.innerHTML = "사용가능한 아이디입니다";
-                checkIdResult.style.color = "green"
-            },
-            error : function() {
-                checkIdResult.innerHTML = "이미 사용중인 이메일입니다";
-                checkIdResult.style.color = "red"
-            }
-        })
-    }
+
 
 </script>
 <main>
--- src/main/webapp/resources/js/join.js
+++ src/main/webapp/resources/js/join.js
@@ -1,36 +1,22 @@
-// const joinForm = document.querySelector("#main_form");
-// const memberId = document.querySelector("#memberId");
-
-// function duCheckId(){
-//     const checkId = memberId.value;
-//     const checkIdResult = document.querySelector("#checkIdResult");
-//     $.ajax({
-//         type: "post",
-//         url: "/search",
-//         data: {
-//             "checkId": checkId
-//         },
-//         success: function(){
-//             checkIdResult.innerHTML = "사용가능한 아이디입니다";
-//             checkIdResult.style.color = "green"
-//         },
-//         error : function() {
-//             checkIdResult.innerHTML = "이미 사용중인 이메일입니다";
-//             checkIdResult.style.color = "red"
-//         }
-//     })
-// }
-
-// memberId.addEventListener("onblur", duCheckId);
-// function handleJoin(event) {
-//     event.preventDefault();
-//     let memberId = document.querySelector("#memberId");
-//     const exp = /^(?=.*[a-z])[a-z\d]{5,10}$/;
-//     if(memberId.match(exp)){
-//
-//     }else {
-//
-//     }
-// }
-
-// joinForm.addEventListener("submit", handleJoin);
+const join = () => {
+    main_form.submit();
+}
+const checkId = () => {
+    const checkId = document.querySelector("#memberId").value;
+    const checkIdResult = document.querySelector("#checkIdResult");
+    $.ajax({
+        type: "post",
+        url: "/search",
+        data: {
+            "checkId": checkId
+        },
+        success: function () {
+            checkIdResult.innerHTML = "사용가능한 아이디입니다";
+            checkIdResult.style.color = "green"
+        },
+        error: function () {
+            checkIdResult.innerHTML = "이미 사용중인 이메일입니다";
+            checkIdResult.style.color = "red"
+        }
+    })
+}
